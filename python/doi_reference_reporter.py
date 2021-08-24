8fb92496b08aa00ede1bf47f61e41be77d30e894
ostwald <ostwald@ucar.edu>
2021-08-24 11:10:58 -0600

    sync_8_24

---
+++ python/doi_reference_reporter.py
@@ -0,0 +1,22 @@
+"""
+similar to smart_partial_txt, create a award tally by collumn so it can be
+compared against smart_partial
+"""
+import os, sys, re, traceback
+from smart_partial_txt import SmartPartialRecord, SmartPartialReader
+
+class DoiRefRecord (SmartPartialRecord):
+    pass
+
+class DoiRefReader (SmartPartialReader):
+    def get_award_id_columns (self):
+        return self.header[-1:]
+
+if __name__ == '__main__':
+    path = '/Users/ostwald/devel/opensky/pubs_to_grants/ARTICLES_award_id_data/DOI-REFERENCE_TABLE.csv'
+    reader = DoiRefReader (path)
+
+    print 'read',len(reader.data)
+
+    print reader.report_tally_map()
+    reader.write_tally_map()
